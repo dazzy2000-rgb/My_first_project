a706cccdb4d2bcd14290b7fc525628fc13834d0f
dazzy2000-rgb <dazzy2000@gmail.com>
2026-01-28 22:09:47 +0200

    pytest example

---
+++ Python_trenning/playwright_example/pytest_example.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class TestPytestExample(unittest.TestCase):
+
+    def test_summery(self):
+        print("test_summery")
+        a=2
+        b=3
+        assert a+b==5, "The sum of two numbers is not expected"
+    def test_qu(self):
+        print("test_qu")
+        a=2
+        b=3
+        assert a*b==6, "The q of two numbers is not expected"
+    def test_minus(self):
+        print("test_minus")
+        a=2
+        b=3
+        assert b-a==1, "The different of two numbers is not expected"
