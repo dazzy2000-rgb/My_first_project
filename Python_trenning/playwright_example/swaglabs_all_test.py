62cd77d7c1a4fac08f9cefb626cfdd2053f49978
dazzy2000-rgb <dazzy2000@gmail.com>
2026-01-30 20:11:22 +0200

    first pytest

--- Python_trenning/playwright_example/swaglabs_all_test.py
+++ Python_trenning/playwright_example/swaglabs_all_test.py
@@ -1,3 +1,5 @@
+import time
+
 
 class TestSwagLabsTest ():
 
@@ -5,3 +7,25 @@ class TestSwagLabsTest ():
         print("test_login")
         page = setup_playwright
         page.goto("https://www.saucedemo.com/")
+        user_name = page.locator("[id='user-name']")
+        user_name.fill("standard_user")
+        password = page.locator("[id='password']")
+        password.fill("secret_sauce")
+        login_button = page.locator("[name='login-button']")
+        login_button.click()
+        time.sleep(3)
+        url = page.url
+        assert url == "https://www.saucedemo.com/inventory.html","Page URL is not expected after Login"
+
+    def test_login(self, setup_playwright):
+        print("test_login")
+        page = setup_playwright
+        page.goto("https://www.saucedemo.com/")
+        user_name = page.locator("[id='user-name']")
+        user_name.fill("standard_user")
+        password = page.locator("[id='password']")
+        password.fill("fake_secret_sauce")
+        login_button = page.locator("[name='login-button']")
+        login_button.click()
+        url = page.url
+        assert url == "https://www.saucedemo.com/", "Page URL is not expected after incorrect Login"
