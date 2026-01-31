7aca2cd4422cad9cc02719d0b1768e1d7e83ee3e
dazzy2000-rgb <dazzy2000@gmail.com>
2026-02-01 01:03:34 +0200

    ebay all test my

--- Python_trenning/playwright_example/ebay_all_test.py
+++ Python_trenning/playwright_example/ebay_all_test.py
@@ -7,3 +7,22 @@ class TestEbayAdvanced ():
         button.click()
         assert page.url == "https://www.ebay.com/sch/ebayadvsearch","Page did not load correctly"
         print("End test")
+
+    def test_ebay_drop_down_example (self,setup_playwright):
+        page = setup_playwright
+        page.goto("https://www.ebay.com/")
+        button = page.get_by_role("link", name="Advanced")
+        button.click()
+        drop_down = page.locator("[id='s0-1-20-4[0]-7[3]-_sacat']")
+        drop_down.select_option(label="Art")
+        print("End test")
+
+    def test_ebay_checkbox_example (self,setup_playwright):
+        page = setup_playwright
+        page.goto("https://www.ebay.com/")
+        button = page.get_by_role("link", name="Advanced")
+        button.click()
+
+        sold = page.locator("[name = 'LH_Sold']")
+        sold.check()
+        print("End test")
