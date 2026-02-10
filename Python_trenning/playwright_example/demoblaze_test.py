e4ee736c4c1bea5154337f2eb73d774261a70420
dazzy2000-rgb <dazzy2000@gmail.com>
2026-02-10 22:36:17 +0200

    demoblaze test

--- Python_trenning/playwright_example/demoblaze_test.py
+++ Python_trenning/playwright_example/demoblaze_test.py
@@ -1,18 +1,11 @@
-import time
 
-from playwright.sync_api import sync_playwright
+class Testdemoblaze():
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://www.demoblaze.com/")
-    contact=page.get_by_role("link", name="Contact")
-    contact.click()
-    page.locator("#exampleModal .modal-footer button.btn-secondary").click()
-    # close_button = page.get_by_role("button", name="Close")
-    # close_button.click()
-    # assert Pass.input_value()=="2"
-    # assert From.input_value() == "London"
-    time.sleep(5)
-    browser.close()
-    print("Test end")
+    def test_demoblaze(self,setup_playwright):
+        page = setup_playwright
+        page.goto("https://www.demoblaze.com/")
+        contact=page.get_by_role("link", name="Contact")
+        contact.click()
+        close_but = page.query_selector_all('[class*="btn-secondary"]')
+        close_but[0].click()
+        print("Test end")
