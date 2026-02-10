73f4745556dd8df27cafccce359d253cafe7aa65
dazzy2000-rgb <dazzy2000@gmail.com>
2026-02-10 23:11:46 +0200

    zara test new

--- Python_trenning/playwright_example/zara_test.py
+++ Python_trenning/playwright_example/zara_test.py
@@ -1,12 +1,10 @@
 
-class Test_zara():
+class Testzara():
 
-    def test_ZaraSaleBucket(self, setup_playwright):
-        page =setup_playwright
-        page.goto("https://www.zara.com")
-        page.locator('[data-qa-action="go-to-store"]').click()
-        # SB=page.locator("[class='layout-header-action__link layout-header-action__link--type-text link']").click()
-        sale_button =page.locator('[data-qa-id="layout-header-go-to-cart"]')
-        sale_button_text =sale_button.inner_text()
-        print(f"text fuond the value is {sale_button_text}")
-        assert "0" in sale_button_text ,"0 was not found in sale bucket text"
+    def test_zara(self, setup_playwright):
+        page = setup_playwright
+        page.goto("https://www.zara.com/il/en/")
+        shopping = page.locator("a.layout-header-desktop-action-cart[data-qa-id='layout-actions-cart']")
+        label = shopping.get_attribute("aria-label")
+        print("aria-label:", label)
+        assert label == "Basket empty"
