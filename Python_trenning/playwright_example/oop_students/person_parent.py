3d39bc2923aff966dc3b72c68a39594ed6b382a1
dazzy2000-rgb <dazzy2000@gmail.com>
2026-02-14 14:17:39 +0200

    students and drivers

---
+++ Python_trenning/playwright_example/oop_students/person_parent.py
@@ -0,0 +1,7 @@
+
+class age_parametr():
+    def age_parametr(self,age):
+        if age > 18:
+            print(f"Student is {age} years old")
+        elif age < 18:
+            print(f"Student is less 18 years old")
---
+++ Python_trenning/playwright_example/oop_students/students_object.py
@@ -0,0 +1,11 @@
+
+class student():
+    def __init__(self,first_name,last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def is_pass(self.grades):
+        summery = 0
+            for grade in grades
+
+
