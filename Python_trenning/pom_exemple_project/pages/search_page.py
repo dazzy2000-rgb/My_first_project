e28d83a997200865b4f593193e2804c8e8feb3e2
dazzy2000-rgb <dazzy2000@gmail.com>
2026-02-14 19:58:13 +0200

    ebay test project

---
+++ Python_trenning/pom_exemple_project/pages/search_page.py
@@ -0,0 +1,22 @@
+
+class SearchPage():
+    def __init__(self, page):
+        self.page = page
+
+    def search_for_items(self,item):
+        search_menu = self.page.locator("[id='gh-ac']")
+        search_menu.click()
+        search_menu.fill(item)
+
+        self.page.get_by_role("button",name="Search",exact=True).click()
+    def get_result_after_search(self):
+        print("Trying to get result after search")
+        text = self.page.locator(".srp-controls__count-heading").inner_text()
+        index =text.index("+")
+        text =text[:index]
+        text = text.replace(",","")
+        print(f"result text is {text}")
+        return text
+
+    def click_on_advanced_link(self):
+        pass
---
+++ Python_trenning/pom_exemple_project/tests/ebay_test.py
@@ -0,0 +1,12 @@
+from PythonProject.Python_trenning.pom_exemple_project.pages.search_page import SearchPage
+
+
+class TestEbayTest:
+    def test_search_for_camera(self,setup_playwright_project):
+        page = setup_playwright_project
+        page.goto("https://www.ebay.com/")
+        search_page = SearchPage(page)
+        search_page.search_for_items("Camera Canon")
+        text = search_page.get_result_after_search()
+        assert int(text)>100
+
