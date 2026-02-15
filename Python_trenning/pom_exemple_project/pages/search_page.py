685f728d4b1a56afc6fb527c5823822782ab43dd
dazzy2000-rgb <dazzy2000@gmail.com>
2026-02-15 18:34:21 +0200

    ebay test project

--- Python_trenning/pom_exemple_project/pages/search_page.py
+++ Python_trenning/pom_exemple_project/pages/search_page.py
@@ -9,9 +9,10 @@ class SearchPage():
         search_menu.fill(item)
 
         self.page.get_by_role("button",name="Search",exact=True).click()
-    def get_result_after_search(self):
+    def get_amount_after_search(self):
         print("Trying to get result after search")
         text = self.page.locator(".srp-controls__count-heading").inner_text()
+
         index =text.index("+")
         text =text[:index]
         text = text.replace(",","")
@@ -19,4 +20,5 @@ class SearchPage():
         return text
 
     def click_on_advanced_link(self):
-        pass
+        adv_button = self.page.get_by_role("link", name="Advanced")
+        adv_button.click()
--- Python_trenning/pom_exemple_project/tests/ebay_test.py
+++ Python_trenning/pom_exemple_project/tests/ebay_test.py
@@ -1,3 +1,6 @@
+from playwright.sync_api import expect
+
+from PythonProject.Python_trenning.pom_exemple_project.pages.adv_page import AdvPage
 from PythonProject.Python_trenning.pom_exemple_project.pages.search_page import SearchPage
 
 
@@ -7,6 +10,22 @@ class TestEbayTest:
         page.goto("https://www.ebay.com/")
         search_page = SearchPage(page)
         search_page.search_for_items("Camera Canon")
-        text = search_page.get_result_after_search()
+        text = search_page.get_amount_after_search()
         assert int(text)>100
 
+    def test_click_on_advanced_link(self,setup_playwright_project):
+        page = setup_playwright_project
+        page.goto("https://www.ebay.com/")
+        search_page = SearchPage(page)
+        search_page.click_on_advanced_link()
+        expect(page).to_have_url("https://www.ebay.com/sch/ebayadvsearch")
+        assert page.title() == "Advanced Search | eBay", "Page title is not as expected after click_on_advanced_link"
+
+    def test_search_from_adv_page(self, setup_playwright_project, ITEM_TO_FIND=None):
+        page = setup_playwright_project
+        adv_page = AdvPage(page)
+        search_page = SearchPage(page)
+        search_page.click_on_advanced_link()
+        adv_page.search_for_item(ITEM_TO_FIND)
+        text = search_page.get_amount_after_search()
+        assert int(text) > 100, "products did not found "
